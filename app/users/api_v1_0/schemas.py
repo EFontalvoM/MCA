406cdb995d1e1ae6898a607b27ee6dd0bf867885
EFontalvoM <78222022+EFontalvoM@users.noreply.github.com>
2022-10-31 08:39:36 -0500

    Version 1.0 ajuste de tipo de viariables
    Se han hecho ajustes de las variables en los Models y en Resources para el fix de los errores de tipo que se habian presentado.

--- app/users/api_v1_0/schemas.py
+++ app/users/api_v1_0/schemas.py
@@ -6,12 +6,12 @@ from app.ext import ma
 class usersSchema(ma.Schema):
     auto = fields.String()
     auto_color = fields.String()
-    auto_modelo = fields.Integer()
-    auto_tipo = fields.Integer()
+    auto_modelo = fields.String()
+    auto_tipo = fields.String()
     avatar = fields.String()
     cantidad_compras_realizadas = fields.Integer()
     codigo_zip = fields.String()
-    color_favorito = fields.Integer()
+    color_favorito = fields.String()
     credit_card_num_cypher = fields.String()
     cuenta_numero = fields.String()
     direccion = fields.String()
@@ -22,16 +22,16 @@ class usersSchema(ma.Schema):
     geo_longitud = fields.String()
     ip = fields.String()
     user_name = fields.String()
-    id = fields.Integer(dump_only=True)
+    id = fields.Integer()
 
 class agentSchema(ma.Schema):
-    id = fields.Integer(dump_only=True)
+    id = fields.Integer()
     email = fields.String()
-    apiKey = fields.Integer(dump_only=True)
+    apiKey = fields.Integer()
     perfil = fields.String()
 
 class perfiles(ma.Schema):
-    id = fields.Integer(dump_only=True)
+    id = fields.Integer()
     nombrePerfil = fields.String()
     
 
